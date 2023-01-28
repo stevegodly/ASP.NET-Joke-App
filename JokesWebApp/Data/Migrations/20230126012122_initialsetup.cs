25495bff0d3e3985881650df283a8eb862314733
stevegodly <stevegodly12@gmail.com>
2023-01-28 22:28:09 +0530

    add c# file

---
+++ JokesWebApp/Data/Migrations/20230126012122_initialsetup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JokesWebApp.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "JokeAnswer",
+                table: "Joke",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JokeAnswer",
+                table: "Joke");
+        }
+    }
+}
